86e8e1f263ed469ff5021d39b9305aa2baad1dc1
johnsiefert <johnsief12@gmail.com>
2022-03-09 17:58:49 -0500

    undo

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,45 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String
+    tvCount: Int
+    savedTv: [Tv]
+}
 
+type Tv {
+    tvId: ID!
+    description: String
+    image: String
+    link: String
+    title: String!
+}
 
+type Auth {
+    token: ID!
+    user: User
+}
+
+input TvInput{
+    description: String!
+    tvId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveTv(tvData: TvInput!): User
+    removeTv(tvId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
