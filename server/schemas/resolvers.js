4e8f5df9b88477183799dab3b165f8118bf57155
johnsiefert <johnsief12@gmail.com>
2022-03-10 16:25:00 -0500

    mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,6 +17,56 @@ const resolvers = {
       },
     },
 
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+
+          const correctPw = await user.isCorrectPassword(password);
+
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+
+          const token = signToken(user);
+          return { token, user };
+        },
+        saveTv: async (parent, { tvData }, context) => {
+          if (context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+              { _id: context.user._id },
+              { $push: { savedTv: tvData } },
+              { new: true }
+            );
+
+            return updatedUser;
+          }
+
+          throw new AuthenticationError('You need to be logged in!');
+        },
+        removeTv: async (parent, { tvId }, context) => {
+          if (context.user) {
+            const updatedUser = await User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedTv: { tvId } } },
+              { new: true }
+            );
+
+            return updatedUser;
+          }
+
+          throw new AuthenticationError('You need to be logged in!');
+        },
+      },
 
 
 };
