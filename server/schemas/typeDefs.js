57ef26aa7339f3c023413c66e93a771238d19bf7
spaghettimedic <dsteiner247@gmail.com>
2022-03-10 20:51:57 -0500

    updated typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,53 @@
-const { gql } = require('apollo-server-express');
-
+// import the gql tagged template function
+const { gql } =  require('apollo-server-express');
+// create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    comments: [Comment]
+    friendsList: [User]
+  }
 
+  type Comment {
+    _id: ID
+    commentText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
 
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    comments(username: String): [Comment]
+    comment(_id: ID!): Comment
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addComment(commentText: String!): Comment
+    addReaction(commentId: ID!, reactionBody: String!): Comment
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
+// export the typeDefs
 module.exports = typeDefs;
