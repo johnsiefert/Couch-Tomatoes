27cba9b10bd1c9f5bb41e74da94bdb3c84cb785e
spaghettimedic <dsteiner247@gmail.com>
2022-03-14 16:26:58 -0400

    Login/Signup page renders when nav link is clicked with form in place - form needs to be tested with backend

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
 
 function Nav() {
 
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>Login/Signup</a></li>
-        <li><a>Profile</a></li>
-      </ul>
-    </nav>
+    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link to="/">
+          <h1>Couch Tomatoes</h1>
+        </Link>
+
+        <nav className="text-center">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>Logout</a>
+            </>
+          ) : (
+            <>
+              <Link to="/loginSignup">Login/Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
   );
 };
 
---
+++ client/src/pages/SingleShow.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SingleShow() {
+  
+};
+
+export default SingleShow;
