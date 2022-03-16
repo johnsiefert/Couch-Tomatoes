a1b84888c6c278c67be832792176df244500f00e
spaghettimedic <dsteiner247@gmail.com>
2022-03-15 21:39:16 -0400

    updated 'homepage' value in package.json and fixed static path to use /build instead of /public

--- server/server.js
+++ server/server.js
@@ -30,11 +30,11 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
